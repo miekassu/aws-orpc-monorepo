823a3fc27eaba737966db1f4d123b7d7dbea2330
miekassu <kasper.hamalainen@gmail.com>
2025-06-16 06:14:16 +0700

    refactor: rename resources

--- apps/infra/lib/infra-stack.ts
+++ apps/infra/lib/infra-stack.ts
@@ -11,15 +11,15 @@ import {
 import { Construct } from 'constructs'
 
 export class InfraStack extends Stack {
-  public readonly entriesTable: aws_dynamodb.TableV2
+  public readonly postsTable: aws_dynamodb.TableV2
   public readonly orpcHandler: aws_lambda_nodejs.NodejsFunction
   public readonly streamingFunction: aws_lambda_nodejs.NodejsFunction
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    // DynamoDB Table for Entries
-    this.entriesTable = new aws_dynamodb.TableV2(this, 'EntriesTable', {
+    // DynamoDB Table for Posts
+    this.postsTable = new aws_dynamodb.TableV2(this, 'PostsTable', {
       partitionKey: {
         name: 'id',
         type: aws_dynamodb.AttributeType.STRING,
@@ -28,7 +28,7 @@ export class InfraStack extends Stack {
     })
 
     // GSI for efficient time-based queries
-    this.entriesTable.addGlobalSecondaryIndex({
+    this.postsTable.addGlobalSecondaryIndex({
       indexName: 'CreatedAtIndex',
       partitionKey: {
         name: 'createdAt',
@@ -49,8 +49,8 @@ export class InfraStack extends Stack {
     )
 
     // Grant DynamoDB permissions
-    this.entriesTable.grantReadWriteData(this.orpcHandler)
-    this.entriesTable.grantReadData(this.streamingFunction)
+    this.postsTable.grantReadWriteData(this.orpcHandler)
+    this.postsTable.grantReadData(this.streamingFunction)
 
     // oRPC only works via function URL
     const oRPCEndpoint = this.streamingFunction.addFunctionUrl({
