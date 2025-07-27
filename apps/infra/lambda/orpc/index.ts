cbf6b8ecc7b0b080d0d0e14d5e2e89f892b85c51
miekassu <kasper.hamalainen@gmail.com>
2025-07-28 06:08:30 +0700

    feat: update lambda endpoint as per latest docs

--- apps/infra/lambda/orpc/index.ts
+++ apps/infra/lambda/orpc/index.ts
@@ -1,5 +1,5 @@
 import { APIGatewayProxyEventV2 } from 'aws-lambda'
-import { experimental_RPCHandler as RPCHandler } from '@orpc/server/aws-lambda'
+import { RPCHandler } from '@orpc/server/aws-lambda'
 import { postsRouter } from './router'
 
 const rpcHandler = new RPCHandler(postsRouter)
@@ -8,22 +8,20 @@ const rpcHandler = new RPCHandler(postsRouter)
  * oRPC only supports [AWS Lambda response streaming](https://aws.amazon.com/blogs/compute/introducing-aws-lambda-response-streaming/).
  * If you need support chunked responses, use a combination of Hono's `aws-lambda` adapter and oRPC.
  */
-export const handler = awslambda.streamifyResponse<APIGatewayProxyEventV2>(
-  async (event, responseStream, _context) => {
-    const { matched } = await rpcHandler.handle(event, responseStream, {
-      prefix: '/rpc',
-      context: {}, // Provide initial context if needed
-    })
+export const handler = awslambda.streamifyResponse<APIGatewayProxyEventV2>(async (event, responseStream, context) => {
+  const { matched } = await rpcHandler.handle(event, responseStream, {
+    prefix: '/rpc',
+    context: {} // Provide initial context if needed
+  })
 
-    if (matched) {
-      return
-    }
-
-    awslambda.HttpResponseStream.from(responseStream, {
-      statusCode: 404,
-    })
-
-    responseStream.write('Not found')
-    responseStream.end()
+  if (matched) {
+    return
   }
-)
+
+  awslambda.HttpResponseStream.from(responseStream, {
+    statusCode: 404,
+  })
+
+  responseStream.write('Not found')
+  responseStream.end()
+})
