6880e423ef55648a210dbdacd60632209b4caf13
miekassu <kasper.hamalainen@gmail.com>
2025-07-28 14:34:58 +0700

    refactor: remove dynamodb to keep things simple

--- apps/infra/lib/infra-stack.ts
+++ apps/infra/lib/infra-stack.ts
@@ -1,39 +1,18 @@
 import {
-  aws_dynamodb,
   aws_lambda,
   aws_lambda_nodejs,
   CfnOutput,
   Duration,
-  RemovalPolicy,
   Stack,
   StackProps,
 } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 
 export class InfraStack extends Stack {
-  public readonly postsTable: aws_dynamodb.TableV2
   public readonly orpcHandler: aws_lambda_nodejs.NodejsFunction
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
-
-    // DynamoDB Table for Posts
-    this.postsTable = new aws_dynamodb.TableV2(this, 'PostsTable', {
-      partitionKey: {
-        name: 'id',
-        type: aws_dynamodb.AttributeType.STRING,
-      },
-      removalPolicy: RemovalPolicy.DESTROY,
-    })
-
-    // GSI for efficient time-based queries
-    this.postsTable.addGlobalSecondaryIndex({
-      indexName: 'CreatedAtIndex',
-      partitionKey: {
-        name: 'createdAt',
-        type: aws_dynamodb.AttributeType.STRING,
-      },
-    })
 
     // CRUD Lambda Function
     this.orpcHandler = new aws_lambda_nodejs.NodejsFunction(
@@ -46,9 +25,6 @@ export class InfraStack extends Stack {
         memorySize: 512,
       }
     )
-
-    // Grant DynamoDB permissions
-    this.postsTable.grantReadWriteData(this.orpcHandler)
 
     // oRPC only works via function URL
     const oRPCEndpoint = this.orpcHandler.addFunctionUrl({
